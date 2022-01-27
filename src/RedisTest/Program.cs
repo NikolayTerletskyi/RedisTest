9e49a8ccf4499e8ecb7f7925e28256fcba279c70
Nikolay <naterletskiy@gmail.com>
2022-01-27 14:16:36 +0200

    base connection

---
+++ src/RedisTest/Program.cs
@@ -0,0 +1,10 @@
+﻿using StackExchange.Redis;
+
+using var redis = ConnectionMultiplexer.Connect("localhost:6379");
+
+Console.WriteLine("Connection estabilished");
+
+var subscriber = redis.GetSubscriber();
+subscriber.PublishAsync("channel1", "hello");
+
+Console.WriteLine("Hello sended");
---
+++ src/RedisTestSub/Program.cs
@@ -0,0 +1,15 @@
+﻿using StackExchange.Redis;
+
+using var redis = ConnectionMultiplexer.Connect("localhost:6379");
+
+Console.WriteLine("Connection estabilished");
+
+var subscriber = redis.GetSubscriber();
+subscriber.Subscribe("channel1", (_, value) =>
+{
+    Console.WriteLine($"I recieve {value}");
+});
+
+Console.WriteLine("Subscribed press any key to exit");
+
+Console.ReadKey();
